5bf41bce23360b2407d1189de804147433705f36
tauhid621 <tauhid621@users.noreply.github.com>
2022-10-26 06:48:49 +0000

    Prettified Code!

--- test/preety.js
+++ test/preety.js
@@ -1,7 +1,5 @@
-var x, y, z;  // Declare 3 variables
-x = 5;    // Assign the value 5 to x
-y =  6;    // Assign the value 6 to y
+var x, y, z; // Declare 3 variables
+x = 5; // Assign the value 5 to x
+y = 6; // Assign the value 6 to y
 
-
-
-z = x + y;  // Assign the sum of x and y to z
+z = x + y; // Assign the sum of x and y to z
